bee8d96630bebf12085286d2c795090fc3e066aa
travor <travor20814@gmail.com>
2016-11-29 21:32:28 +0800

    modify socketio & add dependencies, front-end port

--- api/io.js
+++ api/io.js
@@ -5,6 +5,7 @@ import SocketIo from 'socket.io';
 export function addSocketIO(app) {
   const server = http.Server(app);
   const io = new SocketIo(server);
+  const userName = 'user';
 
   io.on('connection', (socket) => {
     console.info(`New client connected (id=${socket.id}).`);
@@ -12,11 +13,14 @@ export function addSocketIO(app) {
     socket.on('send', (data) => {
       console.log(`id[${socket.id}]: ${data}`);
       socket.broadcast.emit('receive', {
-        id: 1,
+        id: socket.id || 1,
         avatar: 'http://orig06.deviantart.net/8267/f/2012/133/6/f/xcsox_basic_avatar_pack__1__john_cena_starz_by_xcaptainshowoffx-d4zkfdc.jpg',
         name: '江西拿',
-        content: 'you can\' t see me',
+        content: `id[${socket.id}]:${data}`,
       });
+    });
+    socket.emit('receive', {
+      content: `Welcome ${userName}`,
     });
   });
 
